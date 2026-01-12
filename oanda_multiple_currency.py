984acd75ed1219d07d12c3f2701d2009936cb039
ireneqir <56150202+ireneqir@users.noreply.github.com>
2026-01-12 14:01:05 +0800

    Update oanda_multiple_currency.py

--- oanda_multiple_currency.py
+++ oanda_multiple_currency.py
@@ -69,6 +69,7 @@ df.to_excel(filename, index=False)
 EMAIL_USER = os.environ["EMAIL_USER"]
 EMAIL_PASS = os.environ["EMAIL_PASS"]
 
+print(EMAIL_USER)
 msg = EmailMessage()
 msg["Subject"] = "Daily Currency Trigger"
 msg["From"] = EMAIL_USER
@@ -88,3 +89,4 @@ with smtplib.SMTP("smtp.office365.com", 587) as server:
     server.send_message(msg)
 
 print("Email sent successfully")
+
